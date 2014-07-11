7dbbe36ab9d337aec712a7ce18a26cdec650894a
Fred Phillips <fophillips@fophillips.org>
2014-07-11 12:07:57 +0100

    Remove OH params

--- src/simulator/reaction/kinetic/AcidKinetic.java
+++ src/simulator/reaction/kinetic/AcidKinetic.java
@@ -26,14 +26,6 @@ public class AcidKinetic extends IsKineticFactor
 	 */
 	private Double _nH;
 	/**
-	* Rate constant for hydroxide-promoted dissolution
-	*/
-	private Double _kOH;
-	/**
-	 * Reaction order of proton-promoted dissolution
-	 */
-	private Double _nOH;
-	/**
 	 * Activation energy
 	 */
 	private Double _Ea;
@@ -57,58 +49,52 @@ public class AcidKinetic extends IsKineticFactor
 	@Override
 	public void init(Element defMarkUp)
 	{
-		_T = (new XMLParser(defMarkUp)).getParamDbl("T");
+		_T  = (new XMLParser(defMarkUp)).getParamDbl("T");
 		_kH = (new XMLParser(defMarkUp)).getParamDbl("kH");
 		_nH = (new XMLParser(defMarkUp)).getParamDbl("nH");
-		_kOH = (new XMLParser(defMarkUp)).getParamDbl("kOH");
-		_nOH = (new XMLParser(defMarkUp)).getParamDbl("nOH");
 		_Ea = (new XMLParser(defMarkUp)).getParamDbl("Ea");
 		_acidMolarMass = (new XMLParser(defMarkUp)).getParamDbl("acidMolarMass");
 		_ironMolarMass = (new XMLParser(defMarkUp)).getParamDbl("ironMolarMass");
-		nParam = 8;
+		nParam = 6;
 	}
 
 	@Override
 	public void initFromAgent(Element defMarkUp, double[] kineticParam, int paramIndex)
 	{
-		kineticParam[paramIndex] = (new XMLParser(defMarkUp)).getParamDbl("T");
+		kineticParam[paramIndex]   = (new XMLParser(defMarkUp)).getParamDbl("T");
 		kineticParam[paramIndex+1] = (new XMLParser(defMarkUp)).getParamDbl("kH");
 		kineticParam[paramIndex+2] = (new XMLParser(defMarkUp)).getParamDbl("nH");
-		kineticParam[paramIndex+3] = (new XMLParser(defMarkUp)).getParamDbl("kOH");
-		kineticParam[paramIndex+4] = (new XMLParser(defMarkUp)).getParamDbl("nOH");
 		kineticParam[paramIndex+5] = (new XMLParser(defMarkUp)).getParamDbl("Ea");
 		kineticParam[paramIndex+6] = (new XMLParser(defMarkUp)).getParamDbl("acidMolarMass");
 		kineticParam[paramIndex+7] = (new XMLParser(defMarkUp)).getParamDbl("ironMolarMass");
-		nParam = 8;
+		nParam = 6;
 	}
 
 	@Override
 	public double kineticValue(double solute)
 	{
-		Double r = rate(solute, _kH, _nH, _kOH, _nOH, _Ea, _acidMolarMass, _ironMolarMass, _T);
+		Double r = rate(solute, _kH, _nH, _Ea, _acidMolarMass, _ironMolarMass, _T);
 		return r;
 	}
 
 	@Override
 	public double kineticDiff(double solute)
 	{
-		Double dR = diffRate(solute, _kH, _nH, _kOH, _nOH, _Ea, _acidMolarMass, _ironMolarMass, _T);
+		Double dR = diffRate(solute, _kH, _nH, _Ea, _acidMolarMass, _ironMolarMass, _T);
 		return dR;
 	}
 
 	@Override
 	public double kineticValue(double solute, double[] paramTable, int index)
 	{
-		Double T = paramTable[index];
+		Double T  = paramTable[index];
 		Double kH = paramTable[index+1]; 
 		Double nH = paramTable[index+2];
-		Double kOH = paramTable[index+3];
-		Double nOH = paramTable[index+4];
 		Double Ea = paramTable[index+5];
 		Double acidMolarMass = paramTable[index+6];
 		Double ironMolarMass = paramTable[index+7];
 
-		Double r = rate(solute, kH, nH, kOH, nOH, Ea, acidMolarMass, ironMolarMass, T);
+		Double r = rate(solute, kH, nH, Ea, acidMolarMass, ironMolarMass, T);
 
 		return r;
 	}
@@ -116,21 +102,19 @@ public class AcidKinetic extends IsKineticFactor
 	@Override
 	public double kineticDiff(double solute, double[] paramTable, int index)
 	{
-		Double T = paramTable[index];
+		Double T  = paramTable[index];
 		Double kH = paramTable[index+1]; 
 		Double nH = paramTable[index+2];
-		Double kOH = paramTable[index+3];
-		Double nOH = paramTable[index+4];
 		Double Ea = paramTable[index+5];
 		Double acidMolarMass = paramTable[index+6];
 		Double ironMolarMass = paramTable[index+7];
 
-		Double dR = diffRate(solute, kH, nH, kOH, nOH, Ea, acidMolarMass, ironMolarMass, T);
+		Double dR = diffRate(solute, kH, nH, Ea, acidMolarMass, ironMolarMass, T);
 
 		return dR;
 	}
 
-	private Double rate(Double solute, Double kH, Double nH, Double kOH, Double nOH, Double Ea,
+	private Double rate(Double solute, Double kH, Double nH, Double Ea,
 						Double acidMolarMass, Double ironMolarMass, Double T)
 	{
 		Double rate;
@@ -148,7 +132,7 @@ public class AcidKinetic extends IsKineticFactor
 		return rate;
 	}
 
-	private Double diffRate(Double solute, Double kH, Double nH, Double kOH, Double nOH, Double Ea,
+	private Double diffRate(Double solute, Double kH, Double nH, Double Ea,
 							Double acidMolarMass, Double ironMolarMass, Double T)
 	{
 		Double dR;
